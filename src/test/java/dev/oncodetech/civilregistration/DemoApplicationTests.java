643504bc275314840da557250ec4c9634a961f27
AbbasSAFAROV <abbasabdullaziz@gmail.com>
2023-08-04 02:29:25 +0300

    Initial page

---
+++ src/test/java/dev/oncodetech/civilregistration/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oncodetech.civilregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
