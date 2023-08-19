79a016e8a79972731a71ea53ed81066036d04b8d
AbbasSAFAROV <abbasabdullaziz@gmail.com>
2023-08-19 21:23:17 +0300

    Controller updated

--- src/main/java/dev/oncodetech/civilregistration/controller/HomeController.java
+++ src/main/java/dev/oncodetech/civilregistration/controller/HomeController.java
@@ -2,6 +2,7 @@ package dev.oncodetech.civilregistration.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class HomeController {
     @GetMapping()
     public String index(){
         return "Selam Dunya!";
+    }
+
+    @PostMapping
+    public String postMethod(){
+        return "Post created";
     }
 
 }
